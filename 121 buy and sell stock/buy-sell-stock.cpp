5324097f62c3547a1d549e3d1bc31ea2a4b7317c
durvi <bangeradurvi@gmail.com>
2025-07-29 15:25:18 +0530

    Leetcode 121

---
+++ 121 buy and sell stock/buy-sell-stock.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int maxProfit = 0, bestBuy = prices[0];
+    for(int i=1; i<prices.size(); i++){
+        if(prices[i]>bestBuy){
+            maxProfit = max(maxProfit,prices[i]-bestBuy);
+        }
+        bestBuy=min(prices[i],bestBuy);
+    }
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "maximum profit: " << maxProfit(prices) << endl;
+    return 0;
+}
