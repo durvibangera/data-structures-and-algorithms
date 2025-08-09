73a4bf6f0efe6027590627269c06ba41596bd2de
durvi <bangeradurvi@gmail.com>
2025-08-09 11:17:18 +0530

    Leetcode 1910

---
+++ Leetcode/1910 remove all occurrences/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+    while(s.length()>0 && s.find(part)<s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+
+int main() {
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    string result = removeOccurrences(s, part);
+    cout << result << endl;  // Output: "dab"
+    return 0;
+}
