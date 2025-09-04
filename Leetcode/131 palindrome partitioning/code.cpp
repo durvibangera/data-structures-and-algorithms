dccd31d307b7e50fdc09c2021b4a34db04ad251f
durvi <bangeradurvi@gmail.com>
2025-09-04 20:15:02 +0530

    Leetcode 131

---
+++ Leetcode/131 palindrome partitioning/code.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isPali(string s){
+    string s2 = s;
+    reverse(s2.begin(),s2.end());
+    return s==s2;
+}
+
+void getAllParts(string s, vector<string> &partition, vector<vector<string>> &ans){
+    if(s.size()==0){
+        ans.push_back(partition);
+        return;
+    }
+    for(int i=0;i<s.size();i++){
+        string part = s.substr(0,i+1);
+        if(isPali(part)){
+            partition.push_back(part);
+            getAllParts(s.substr(i+1),partition,ans);
+            partition.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string s) {
+    vector<vector<string>> ans;
+    vector<string> partition;
+    getAllParts(s, partition, ans);
+    return ans;
+}
+
+int main(){
+    string s = "aab";
+    vector<vector<string>> ans = partition(s);
+    for(auto v:ans){
+        for(auto str:v){
+            cout<<str<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
